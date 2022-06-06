3bde4027e99184ffc65b57950c185092355e7d3c
rosalinb <rosalinbiswal009@gmail.com>
2022-06-06 14:57:01 +1000

    commit the likebutton

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const express = require('express')
-const app = express()
-const port = 3005
+const express = require("express");
+const app = express();
+const port = 3005;
 
-app.listen(port, () => console.log(`server listening on port ${port}`))
+app.listen(port, () => console.log(`server listening on port ${port}`));
+
+console.log("hi");
